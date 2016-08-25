8b7ca0354499cadb3f187eb9c1b18c6a617e4994
SplittyDev <splittydev@gmail.com>
2016-08-25 03:42:27 +0200

    Improve style

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let win;
 
 function createWindow () {
   win = new BrowserWindow({ width: 400, height: 600 });
-  win.loadURL(`file://${__dirname}/index.html`);
+  win.loadURL(`file://${__dirname}/app/app.html`);
   win.on('closed', () => {
     win = null;
   });
